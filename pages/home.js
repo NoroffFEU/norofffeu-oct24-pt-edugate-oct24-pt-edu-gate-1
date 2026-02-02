a0cbfbdc9f5ab34014034a2baba764a81e064857
Kim T Henriksen <kim.t.henriksen@gmail.com>
2026-02-02 21:51:47 +0100

    add route and page for login

--- pages/home.js
+++ pages/home.js
@@ -1,9 +1,9 @@
 export default function Home() {
-    return /*HTML*/`
+  return /*HTML*/ `
     <div>
         <h1>Home</h1>
         <nav>
-            <a href="/test" data-link>Go to Test Page</a>
+            <a href="/login" data-link>Go to login</a>
             <br><br>
             <a href="/non-existent-page" data-link>Go to 404 Page</a>
         </nav>
