22ed8fc5b70984a5305a51b9345eb17c6effcdf9
Kim T Henriksen <kim.t.henriksen@gmail.com>
2026-02-03 12:25:31 +0100

    test update

--- router/routes.js
+++ router/routes.js
@@ -1,10 +1,12 @@
 // views
 import Home from '../pages/home.js'
 import Test from '../pages/test.js'
+import Login from `../pages/login.js`
 
 const routes = [
     { path: "/", view: Home },
     { path: "/test", view: Test },
+    { path: "/login", view: Login },
 ];
 
 export default routes;
