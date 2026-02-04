3a51e73f213bd6527fb96793bf9f0b0f12e63ec3
Kim T Henriksen <kim.t.henriksen@gmail.com>
2026-02-04 11:11:23 +0100

    add validation for form
    validation for email and password
    
    routed to dashboard if successful, else alert

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,22 @@
 export default function Login() {
+  setTimeout(() => {
+    const button = document.querySelector("#login-btn");
+
+    button.addEventListener("click", function () {
+      const emailInput = document.querySelector("#email");
+      const passwordInput = document.querySelector("#password");
+
+      if (
+        emailInput.value === "teacher@edugate.com" &&
+        passwordInput.value === "password1"
+      ) {
+        window.location.href = "/dashboard";
+      } else {
+        alert("Unsuccesful verification. Wrong email or password");
+      }
+    });
+  }, 0);
+
   return /*HTML*/ `
     <section class="login">
   <div class="login-card">
@@ -8,6 +26,7 @@ export default function Login() {
     <label for="email">Email</label>
     <input type="email" 
     id="email" 
+    name="email"
     placeholder="xxxx@mail.com" 
     required />
 
@@ -20,7 +39,7 @@ export default function Login() {
       required
     />
 
-    <button type="submit">Submit</button>
+    <button id="login-btn" type="button">Submit</button>
   </form>
   </div>
   </section>
