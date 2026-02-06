5c8c5502795f76b22b833cc60f55e210186684d4
Kim T Henriksen <kim.t.henriksen@gmail.com>
2026-02-06 13:34:15 +0100

    add contact form
    form for visitor-contact page

--- pages/contact.js
+++ pages/contact.js
@@ -1,7 +1,47 @@
 export default function Contact() {
   return /* HTML */ `
     <section class="contact">
-      <h1>Contact</h1>
+     <h1>Contact</h1>
+
+     <form id="contact-form">
+
+      <div class="form-field">
+        <label for="email">Email</label>
+        <input
+         type="email"
+         id="email"
+         placeholder="example@mail.com"
+         required
+      />
+      </div>
+
+      <div class="form-field">
+        <label for="name">Name</label>
+        <input
+         type="text"
+         id="name"
+         placeholder="Ola"
+         required
+        />
+      </div>
+
+      <div class="form-field">
+        <label for="surname">Surname</label>
+        <input 
+         type="text" 
+         id="surname" 
+         placeholder="Nordmann" 
+         required
+        />
+
+      <div class="form-field">
+        <label for="message">Message</label>
+        <textarea id="message" placeholder="Your message..." required></textarea>
+      </div>
+
+      <button type="submit">Submit</button>
+     </form>
+
     </section>
   `;
 }
