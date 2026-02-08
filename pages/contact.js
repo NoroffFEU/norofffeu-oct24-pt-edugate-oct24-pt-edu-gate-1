40e132626ac8f42bb9ef346e4221c155b3c2db74
Kim T Henriksen <kim.t.henriksen@gmail.com>
2026-02-08 08:43:11 +0100

    style contact page to match design
    same structure as login form

--- pages/contact.js
+++ pages/contact.js
@@ -1,47 +1,47 @@
 export default function Contact() {
   return /* HTML */ `
     <section class="contact">
-     <h1>Contact</h1>
+      <div class="contact-card">
+        <h1>Contact</h1>
 
-     <form id="contact-form">
+        <p>
+          If you have any questions or feedback, please use the contact form. We
+          would love to hear from you
+        </p>
 
-      <div class="form-field">
-        <label for="email">Email</label>
-        <input
-         type="email"
-         id="email"
-         placeholder="example@mail.com"
-         required
-      />
+        <form id="contact-form">
+          <div class="form-field">
+            <label for="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              placeholder="example@mail.com"
+              required
+            />
+          </div>
+
+          <div class="form-field">
+            <label for="name">Name</label>
+            <input type="text" id="name" placeholder="Ola" required />
+          </div>
+
+          <div class="form-field">
+            <label for="surname">Surname</label>
+            <input type="text" id="surname" placeholder="Nordmann" required />
+          </div>
+
+          <div class="form-field">
+            <label for="message">Message</label>
+            <textarea
+              id="message"
+              placeholder="Your message..."
+              required
+            ></textarea>
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
       </div>
-
-      <div class="form-field">
-        <label for="name">Name</label>
-        <input
-         type="text"
-         id="name"
-         placeholder="Ola"
-         required
-        />
-      </div>
-
-      <div class="form-field">
-        <label for="surname">Surname</label>
-        <input 
-         type="text" 
-         id="surname" 
-         placeholder="Nordmann" 
-         required
-        />
-
-      <div class="form-field">
-        <label for="message">Message</label>
-        <textarea id="message" placeholder="Your message..." required></textarea>
-      </div>
-
-      <button type="submit">Submit</button>
-     </form>
-
     </section>
   `;
 }
