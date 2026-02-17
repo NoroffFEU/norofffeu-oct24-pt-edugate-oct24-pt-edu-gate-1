07de2504e8a6693c5d96e3cafd754e94906cd398
Kim T Henriksen <kim.t.henriksen@gmail.com>
2026-02-17 19:44:42 +0100

    Add sign up form

---
+++ pages/signup.js
@@ -0,0 +1,68 @@
+export default function SignUp() {
+  return /* HTML */ `
+    <section class="signup">
+      <div class="signup-card">
+        <h1>Sign up</h1>
+
+        <form id="signup-form">
+          <div class="form-field">
+            <label for="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="example@mail.com"
+              required
+            />
+          </div>
+
+          <div class="form-field">
+            <label for="firstname">Firstname</label>
+            <input
+              type="text"
+              id="firstname"
+              name="firstname"
+              placeholder="Ola"
+              required
+            />
+          </div>
+
+          <div class="form-field">
+            <label for="surname">Surname</label>
+            <input
+              type="text"
+              id="surname"
+              name="surname"
+              placeholder="Nordmann"
+              required
+            />
+          </div>
+
+          <div class="form-field">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="Choose password"
+              required
+            />
+          </div>
+
+          <div class="form-field">
+            <label for="confirm-password">Confirm password</label>
+            <input
+              type="password"
+              id="confirm-password"
+              name="confirm-password"
+              placeholder="Repeat password"
+              required
+            />
+          </div>
+
+          <button id="signup-btn" type="submit">Submit</button>
+        </form>
+      </div>
+    </section>
+  `;
+}
