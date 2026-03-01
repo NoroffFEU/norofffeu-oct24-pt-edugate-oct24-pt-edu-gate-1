9e9f39fb2386c3471a52726f3fe747e11761bd99
Kim T Henriksen <kim.t.henriksen@gmail.com>
2026-03-01 20:10:17 +0100

    Create registration.js

---
+++ pages/registration.js
@@ -0,0 +1,73 @@
+export default function Registration() {
+  return /*HTML*/ `
+
+    <section class="registration">
+     <div class="registration-card">
+    <h1>Register</h1>
+
+    <form id="registration-form">
+     
+     <div class="form-field">
+      <label for="school">School</label>
+      <input
+       type="text"
+       id="school"
+       name="school"
+       placeholder="Edugate"
+       required
+       />
+     </div>
+
+     <div class="form-field">
+      <label for="address1">Address line 1</label>
+      <input
+       type="text"
+       id="address1"
+       name="address1"
+       placeholder="Address"
+       required
+       />
+     </div>
+
+     <div class="form-field">
+      <label for="address2">Address line 2</label>
+      <input
+       type="text"
+       id="address2"
+       name="address2"
+       placeholder="Address"
+       />
+     </div>
+    
+    <div class="form-container"> 
+     <div class="form-field">
+      <label for="city">City</label>
+      <input
+       type="text"
+       id="city"
+       name="city"
+       placeholder="Oslo"
+       required
+       />
+     </div>
+
+     <div class="form-field">
+      <label for="postal">Postal Code</label>
+      <input
+       type="text"
+       id="postal"
+       name="postal"
+       placeholder="0000"
+       required
+       />
+     </div>
+    </div>
+
+    <button id="registration-btn" type="button">Submit</button>
+
+
+    </form>
+  </div>
+ </section>
+`;
+}
