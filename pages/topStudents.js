942e9704bc1b9cb473e8a9f6b1dd12c931553ae6
Kim T Henriksen <kim.t.henriksen@gmail.com>
2026-03-17 21:15:17 +0100

    add top students table with result data

--- pages/topStudents.js
+++ pages/topStudents.js
@@ -1,4 +1,47 @@
+import { createTable } from "../components/table/table.js";
+import { getUser, login } from "../src/auth.js";
+
 export default function TopStudents() {
+  let user = getUser();
+
+  if (!user) {
+    const savedUser = JSON.parse(localStorage.getItem("user"));
+
+    if (savedUser) {
+      login(savedUser);
+      user = getUser();
+    }
+  }
+
+  if (!user) {
+    history.pushState(null, null, "/login");
+    window.dispatchEvent(new PopStateEvent("popstate"));
+    return "";
+  }
+
+  setTimeout(async () => {
+    const container = document.querySelector("#students-table");
+
+    const response = await fetch("/Data/Results.json");
+    const data = await response.json();
+
+    const tableData = data.results.map((result) => ({
+      studentId: result.studentId,
+      year: result.session,
+      subject: result.subjects[0].name,
+      grade: result.subjects[0].grade,
+    }));
+
+    const table = createTable(container, [
+      { value: "studentId", class: "student-id" },
+      { value: "year", class: "year" },
+      { value: "subject", class: "subject" },
+      { value: "grade", class: "grade" },
+    ]);
+
+    table.render(tableData);
+  }, 0);
+
   return /* HTML */ `
     <section class="top-students-page">
       <div class="top-student-content">
