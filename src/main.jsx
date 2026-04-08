0980b898bd351653308157fe3ca2672c1a6d87cd
Kim T Henriksen <kim.t.henriksen@gmail.com>
2026-04-08 20:58:26 +0200

    setup react app with routing

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./App.jsx";
+import "../style.css";
+
+ReactDom.createRoot(document.getElementById("app")).render(<App />);
