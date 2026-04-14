6dd3a244a013cff6b7d2dd0735e5f632aec1cf21
Adrian de Souza <adriandesouza1@gmail.com>
2026-04-14 07:47:21 +0200

    Revert "swaps header and footer comps"
    This reverts commit 79797d45e78d99a4ee701a34ae7af931e3e1f696.

---
+++ components/header.js
@@ -0,0 +1,15 @@
+export default function Header() {
+  return /*HTML*/ `
+  <div class="navbar">
+    <ul>
+     
+
+    <a href="/dashboard" data-link ><li >Dashboard</li></a>
+    <a href="/about" data-link><li >About</li></a>
+   
+    <a href="/contact" data-link ><li>Contact</li></a>
+    </ul>
+    <img class="hamburger-menu" src="../public/icons/hamburger-menu.png">
+  </div>
+  `;
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "../pages/landing";
-import About from "../pages/about";
-import Footer from "../components/Footer";
-import Header from "../components/Header";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Landing from '../pages/landing';
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/about" element={<About />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
-  );
+  )
 }
