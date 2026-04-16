4e59e500ebcff22f6ff8dc8d6f1c5b62f326abf1
Adrian de Souza <adriandesouza1@gmail.com>
2026-04-16 11:27:27 +0200

    add header and footer

---
+++ components/Header.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export default function Header({ listOfLinks }) {
+  return (
+    <header className="navbar">
+      <ul>
+        {listOfLinks.map((link) => (
+          <li key={link.path}>
+            <Link to={link.path}>{link.name}</Link>
+          </li>
+        ))}
+      </ul>
+      <img
+        className="hamburger-menu"
+        src="../public/icons/hamburger-menu.png"
+      />
+    </header>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Landing from '../pages/landing';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "../pages/landing";
+import About from "../pages/about";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+const headerLinks = [
+  { path: "/dashboard", name: "Dashboard" },
+  { path: "/about", name: "About" },
+  { path: "/contact", name: "Contact" },
+];
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header listOfLinks={headerLinks} />
       <Routes>
         <Route path="/" element={<Landing />} />
+        <Route path="/about" element={<About />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
